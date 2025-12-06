7ea227b13ae747b2716d04648f9a2f1f583a2f48
Shivang Chauhan <shivangchauhan055@gmail.com>
2025-12-06 20:40:17 +0530

    notes

--- src/com/Arrays_Algo/Moore_Voting.java
+++ src/com/Arrays_Algo/Moore_Voting.java
@@ -1,4 +1,17 @@
 package com.Arrays_Algo;
 
 public class Moore_Voting {
+    public static void main(String[] args) {
+        /*
+Majority Element = A number that appears more than n/2 times
+in an array of length n.
+
+Example:
+Input:  [2, 2, 1, 1, 1, 2, 2]
+Output: 2       (because 2 appears 4 times > n/2)
+
+The question guarantees that a majority element exists.
+(LeetCode 169)
+         */
+    }
 }
